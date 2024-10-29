a0458a212257901a28074162db05c457c911d0f8
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2024-10-29 14:07:56 +0530

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,7 +3,7 @@ import video1 from "../assets/video1.mp4"
 import video2 from "../assets/video2.mp4"
 import video3 from "../assets/New Project - Made with Clipchamp.mp4"
 import video4 from "../assets/Untitled video - Made with Clipchamp (1) (1).mp4"
-import video5 from "../assets/img3.mp4"
+import video5 from "../assets/video5.mp4"
 
 
 const Projects = () => {
